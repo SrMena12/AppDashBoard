fae392bdd5ccc3e42e574b23680ad7812797e3f1
SrMena12 <mena121997@gmail.com>
2023-05-24 13:35:12 +0200

    tokens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  product :any;
   title = 'Dashboard';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    const headers = { 'Authorization' : 'my-token'}
+    this.http.get<any>('https://testapi.jasonwatmore.com/products/1', {headers})
+    .subscribe(data => this.product = data);
+  }
 }
